4a8dbf5b62a285acf392fb50f88499b79ec0dd86
David <davvan@pdx.edu>
2023-03-17 23:06:42 -0700

    Created an activity using Intent.

---
+++ airline-android/app/src/main/java/edu/pdx/cs410J/davvan/CalculatorActivity.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.davvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+    public void returnToMain() {
+        finish();
+    }
+}
