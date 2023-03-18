964f653cb900f3ad5dda60533fce8e28bcb05035
David <davvan@pdx.edu>
2023-03-18 10:08:35 -0700

    Lists Arrays in main page

--- airline-android/app/src/main/java/edu/pdx/cs410J/davvan/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/davvan/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listofSums= findViewById(R.id.sums);
+        Integer [] numbers = {1,2,3,4};
+        ArrayAdapter<Integer> sums = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numbers);
+        listofSums.setAdapter(sums);
     }
+
+
 
     public void launchCalculator(View view) {
         startActivity(new Intent(this, CalculatorActivity.class));
