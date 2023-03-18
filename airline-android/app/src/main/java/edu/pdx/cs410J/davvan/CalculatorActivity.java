79bade7a21038ed0d23cbf8661fe86e84ce6ffeb
David <davvan@pdx.edu>
2023-03-17 23:14:02 -0700

    Moved the calculator to second Activity.

--- airline-android/app/src/main/java/edu/pdx/cs410J/davvan/CalculatorActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/davvan/CalculatorActivity.java
@@ -3,6 +3,10 @@ package edu.pdx.cs410J.davvan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -14,5 +18,30 @@ public class CalculatorActivity extends AppCompatActivity {
 
     public void returnToMain() {
         finish();
+    }
+
+    public void computeSum(View view) {
+        EditText leftOperandEditText = findViewById(R.id.leftOperand);
+        EditText rightOperandEditText = findViewById(R.id.rightOperand);
+
+        String leftOperandString= leftOperandEditText.getText().toString();
+        String rightOperandString = rightOperandEditText.getText().toString();
+
+        int leftOperand;
+        int rightOperand;
+        try {
+            leftOperand = Integer.parseInt(leftOperandString);
+            rightOperand = Integer.parseInt(rightOperandString);
+        }catch(NumberFormatException e){
+            Toast.makeText(this, "Invalid number: ", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        int sum = leftOperand + rightOperand;
+
+        TextView sumEditText = findViewById(R.id.sum);
+
+        sumEditText.setText(String.valueOf(sum), TextView.BufferType.EDITABLE);
     }
 }
--- airline-android/app/src/main/java/edu/pdx/cs410J/davvan/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/davvan/MainActivity.java
@@ -21,32 +21,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, CalculatorActivity.class));
     }
 
-    public void computeSum(View view) {
-        EditText leftOperandEditText = findViewById(R.id.leftOperand);
-        EditText rightOperandEditText = findViewById(R.id.rightOperand);
 
-        String leftOperandString= leftOperandEditText.getText().toString();
-        String rightOperandString = rightOperandEditText.getText().toString();
-
-        int leftOperand;
-        int rightOperand;
-        try {
-            leftOperand = Integer.parseInt(leftOperandString);
-            rightOperand = Integer.parseInt(rightOperandString);
-        }catch(NumberFormatException e){
-            Toast.makeText(this, "Invalid number: ", Toast.LENGTH_SHORT).show();
-            return;
-
-        }
-
-
-        int sum = leftOperand + rightOperand;
-
-        TextView sumEditText = findViewById(R.id.sum);
-
-        sumEditText.setText(String.valueOf(sum), TextView.BufferType.EDITABLE);
-
-
-
-    }
 }
